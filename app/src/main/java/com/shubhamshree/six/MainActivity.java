a1401fcb52453f24d479294fa32b3c9abb49256c
Shubham <shubhamshree10@gmail.com>
2020-12-14 09:05:01 +0530

    Working, increment/decrement counter, required runs & run rate calculation

--- app/src/main/java/com/shubhamshree/six/MainActivity.java
+++ app/src/main/java/com/shubhamshree/six/MainActivity.java
@@ -1,36 +1,71 @@
+
 package com.shubhamshree.six;
 
 import androidx.appcompat.app.AppCompatActivity;
 import java.time.*;
-import java.util.*;
+import java.time.temporal.ChronoUnit;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
+    String difference;
+    Period diff;
+    private Button button_plus , button_minus ;
+    int count = 0;
+    long noOfDaysBetween;
+    private TextView run_scored;
+    private TextView runs ;
+    private TextView req_run_rate ;
+
     void findDifference(LocalDate start_date,LocalDate end_date)
     {
-        Period diff= Period.between(start_date, end_date);
-        //Integer.toString(diff.getYears())+ Integer.toString(diff.getMonths())+
+         diff= Period.between(start_date, end_date);
+        noOfDaysBetween = ChronoUnit.DAYS.between(start_date, end_date);
         String overs;
-        if(diff.getDays()>1) {
-             overs = " Overs";
-        }
+        if(noOfDaysBetween>1) {
+             overs = " Overs";}
         else{
-             overs = " Over";
-        }
-              String difference =  Integer.toString(diff.getDays()) + overs ;
+             overs = " Over";}
+        difference =  Long.toString(noOfDaysBetween) + overs ;
         TextView textViewDate = findViewById(R.id.days_remaining);
         textViewDate.setText(difference);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+         button_plus = findViewById(R.id.button_plus);
+         button_minus = findViewById(R.id.button_minus);
+        run_scored = findViewById(R.id.run_scored);
+        runs = findViewById(R.id.runs) ;
+        req_run_rate = findViewById(R.id.req_run_rate) ;
         LocalDate start_date = LocalDate.now(); ;
-        LocalDate end_date = LocalDate.of(2020, 12, 31);
-
+        LocalDate end_date = LocalDate.of(2021, 1, 15);
         findDifference(start_date,end_date);
 
+        button_plus.setOnClickListener(view -> {
+            count++;
+            int req = 200 - count;
+            float rrr = (float)req/(float)noOfDaysBetween;
+            double roundOff = (double) Math.round(rrr * 100) / 100;
+            run_scored.setText(Integer.toString(count));
+            runs.setText(Integer.toString(req));
+            req_run_rate.setText(Double.toString(roundOff) + "/Over");
+        });
+        button_minus.setOnClickListener(view -> {
+            count--;
+            int req = 200 - count;
+            float rrr = (float)req/(float)noOfDaysBetween;
+            double roundOff = (double) Math.round(rrr * 100) / 100;
+            run_scored.setText(Integer.toString(count));
+            runs.setText(Integer.toString(req));
+            req_run_rate.setText(Double.toString(roundOff)+ "/Over");
+        });
     }
 }
